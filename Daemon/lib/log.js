654f1a495bcd11231604a7403e9037db4dfbe5db
FanTaSyLin <fanlin19830908@hotmail.com>
2015-11-03 10:06:07 +0800

    修改了在LINUX下日期格式错误的问题

--- Daemon/lib/log.js
+++ Daemon/lib/log.js
@@ -20,12 +20,20 @@ exports.error = function(provider, msg){
 }
 
 function writeLog(provider, msg, type){
-    var date = new Date().toLocaleDateString();
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var MM = date.getMonth();
+    var dd = date.getDate();
+    var HH = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+    var dateStr = yyyy + '-' + MM + '-' + dd;
+    var timeStr = HH + ':' + mm + ':' + ss;
     var pathname = path.resolve(__dirname, '../log');
-    var logfilePath = pathname + '/' + date + '.log';
+    var logfilePath = pathname + '/' + dateStr + '.log';
     var o ={
         'type' : type,
-        'time' : new Date().toLocaleTimeString(),
+        'time' : timeStr,
         'provider' : provider,
         'msg' : msg
     };
